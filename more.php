01be48e05ca0bf7b78569b64254faa66392ae6e0
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-10-12 19:12:43 -0500

    Added another example file.

--- more.php
+++ more.php
@@ -1,1 +1,48 @@
 <?php
+
+interface AdobeInterface
+{
+    public function design($project);
+}
+
+class Illustrator implements AdobeInterface
+{
+    public function design($project)
+    {
+        echo "An Adobe Illustrator file is designed.\n";
+    }
+}
+
+class Photoshop implements AdobeInterface
+{
+    public function design($project)
+    {
+        echo "An Adobe Photoshop file is designed.\n";
+    }
+}
+
+class InDesign implements AdobeInterface
+{
+    public function design($project)
+    {
+        echo "An Adobe InDesign file is designed.\n";
+    }
+}
+
+class Designer
+{
+    public function design(AdobeInterface $adobe)
+    {
+        $adobe->design($project);
+    }
+}
+
+$illustrator = new Illustrator;
+$photoshop = new Photoshop;
+$indesign = new InDesign;
+
+$designer = new Designer;
+
+$designer->design($illustrator);
+$designer->design($photoshop);
+$designer->design($indesign);
